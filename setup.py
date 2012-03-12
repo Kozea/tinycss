9505cc6f187fe3b524a83e7288071a8ea0373cc0
Simon Sapin <simon.sapin@exyr.org>
2012-03-12 16:38:30 +0100

    Change setup.py to try without Cython and fallback on pure-python
    The cython-generated .c file is included in sdist tarballs with
    the MANIFEST.in file. (Make sure to run Cython befor sdist!)
    
    If Cython is not installed, setup.py will fall back on using the
    pre-generated .c file.
    
    If even that fails, install anyway without the speedups.
    
    This is inspired from the setup.py files for lxml and simplejson.

--- setup.py
+++ setup.py
@@ -1,27 +1,100 @@
-from __future__ import with_statement
 import re
+import sys
 import os.path
 from setuptools import setup, find_packages, Extension
-from Cython.Distutils import build_ext
+from distutils.errors import CCompilerError, DistutilsExecError, \
+    DistutilsPlatformError
+try:
+    from Cython.Distutils import build_ext
+    import Cython.Compiler.Version
+    CYTHON_INSTALLED = True
+except ImportError:
+    from setuptools.command.build_ext import build_ext
+    CYTHON_INSTALLED = False
 
 
-init_py = os.path.join(os.path.dirname(__file__), 'tinycss', '__init__.py')
-with open(init_py) as fd:
+ext_errors = (CCompilerError, DistutilsExecError, DistutilsPlatformError)
+if sys.platform == 'win32' and sys.version_info > (2, 6):
+   # 2.6's distutils.msvc9compiler can raise an IOError when failing to
+   # find the compiler
+   ext_errors += (IOError,)
+
+class BuildFailed(Exception):
+    pass
+
+class ve_build_ext(build_ext):
+    # This class allows C extension building to fail.
+
+    def run(self):
+        try:
+            build_ext.run(self)
+        except DistutilsPlatformError:
+            raise BuildFailed
+
+    def build_extension(self, ext):
+        try:
+            build_ext.build_extension(self, ext)
+        except ext_errors:
+            raise BuildFailed
+
+
+fd = open(os.path.join(os.path.dirname(__file__), 'tinycss', '__init__.py'))
+try:
     VERSION = re.search("VERSION = '([^']+)'", fd.read()).group(1)
+finally:
+    fd.close()
 
 
-setup(
-    name='tinycss',
-    version=VERSION,
-    url='https://github.com/SimonSapin/tinycss',
-    license='BSD',
-    author='Simon Sapin',
-    author_email='simon.sapin@exyr.org',
-    description='A CSS parser, and nothing else.',
-    packages=find_packages(),
-    cmdclass = {'build_ext': build_ext},
-    ext_modules=[Extension(
-        'tinycss.speedups',
-        sources=[os.path.join('tinycss', 'speedups.pyx')]
-    )],
-)
+def run_setup(with_extension):
+    if with_extension:
+        extension_path = os.path.join('tinycss', 'speedups')
+        if CYTHON_INSTALLED:
+            extension_path += '.pyx'
+            print('Building with Cython %s.' % Cython.Compiler.Version.version)
+        else:
+            extension_path += '.c'
+            if not os.path.exists(extension_path):
+                print ("WARNING: Trying to build without Cython, but "
+                       "pre-generated '%s' does not seem to be available."
+                       % extension_path)
+            else:
+                print ('Building without Cython.')
+        kwargs = dict(
+            cmdclass=dict(build_ext=ve_build_ext),
+            ext_modules=[Extension('tinycss.speedups',
+                                   sources=[extension_path])],
+        )
+    else:
+        kwargs = dict()
+
+    setup(
+        name='tinycss',
+        version=VERSION,
+        url='https://github.com/SimonSapin/tinycss',
+        license='BSD',
+        author='Simon Sapin',
+        author_email='simon.sapin@exyr.org',
+        description='A CSS parser, and nothing else.',
+        packages=find_packages(),
+        **kwargs
+    )
+
+
+IS_PYPY = hasattr(sys, 'pypy_translation_info')
+try:
+    run_setup(not IS_PYPY)
+except BuildFailed:
+    BUILD_EXT_WARNING = ('WARNING: The extension could not be compiled, '
+                         'speedups are not enabled.')
+    print('*' * 75)
+    print(BUILD_EXT_WARNING)
+    print('Failure information, if any, is above.')
+    print('Retrying the build without the Cython extension now.')
+    print('*' * 75)
+
+    run_setup(False)
+
+    print('*' * 75)
+    print(BUILD_EXT_WARNING)
+    print('Plain-Python installation succeeded.')
+    print('*' * 75)
