80222295a8d9089c10f827ca6fa589f821877828
Guillaume Ayoub <guillaume.ayoub@kozea.fr>
2016-09-14 18:08:37 +0200

    Fix tests with isort on Python 2
    Isort creators are nicely allowed to go to hell for their pie_slice
    module.

--- tinycss/tests/__init__.py
+++ tinycss/tests/__init__.py
@@ -10,6 +10,14 @@
 
 from __future__ import unicode_literals
 
+import sys
+
+
+# Awful workaround to fix isort's "sys.setdefaultencoding('utf-8')".
+if sys.version_info[0] == 2:
+    reload(sys)  # noqa
+    sys.setdefaultencoding('ascii')
+
 
 def assert_errors(errors, expected_errors):
     """Test not complete error messages but only substrings."""
